4c768e13bd3de825e450ee1cb5f4a12252544503
Prasun <prasunsapkota@gmail.com>
2023-12-28 20:29:48 +0545

    Scene two gate and vortex added

--- Lallu/game/Scene/Scene_2.gd
+++ Lallu/game/Scene/Scene_2.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _ready():
 	$openMap.visible = false
 	$closeMap.visible = true
-	$Vortex.visible = false
+	$openMap/vortex/Vortex.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,9 +17,11 @@ func _on_egg_body_exited(body):
 	$closeMap.clear()
 	$openMap.visible = true
 	$closeMap.visible = false
+	$openMap/vortex/Vortex.visible = false
+	
 	
 	print("Detected")
 
 
-func _on_area_2d_body_entered(body):
-	$Vortex.visible = true
+func _on_area_2d_body_entered(CharacterBody2D):
+	$openMap/vortex/Vortex.visible = true
