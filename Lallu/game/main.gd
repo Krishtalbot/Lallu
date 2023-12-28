9a699aa334acb7f78c55ef5e3b5c0dad2471d2bd
Prasun <prasunsapkota@gmail.com>
2023-12-28 10:39:05 +0545

    Added collision on fence and scene 2 added

--- Lallu/game/main.gd
+++ Lallu/game/main.gd
@@ -13,3 +13,7 @@ func _process(delta):
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_scene_2_pressed():
+	get_tree().change_scene_to_file("res://Scene/Scene_2.tscn")
